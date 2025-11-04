f8c21797c9dbeeb7da1067e39695b40fba26f823
Marek <marek.harjumaki@metropolia.fi>
2025-11-04 20:12:04 +0200

    valmis

--- marggu/Laskin.py
+++ marggu/Laskin.py
@@ -1,19 +1,22 @@
-print("valitse mitä toimintoa haluat käyttää:\n a:yhteenlasku\n b:vähennyslasku\n c:jakolasku\n d:kertolasku")
+while True:
+    print("valitse mitä toimintoa haluat käyttää:\n a:yhteenlasku\n b:vähennyslasku\n c:jakolasku\n d:kertolasku")
 
-valinta = input("valintasi (a-d):")
+    valinta = input("valintasi (a-d):")
 
-a = float(input("anna ensimmäinen luku "))
-b = float(input("anna toinen luku "))
+    if valinta == "Q":
+        break
+    a = float(input("anna ensimmäinen luku "))
+    b = float(input("anna toinen luku "))
 
-if valinta == "a":
-    print("yhteenlasku", a + b)
-elif valinta == "b":
-    print("vähennyslasku", a - b)
-elif valinta == "c":
-    print("jakolasku", a / b)
-elif valinta =="d":
-    print("kertolasku", a * b)
-else:
-    print("virheellinen valinta")
+    if valinta == "a":
+        print("yhteenlasku", a + b)
+    elif valinta == "b":
+        print("vähennyslasku", a - b)
+    elif valinta == "c":
+        print("jakolasku", a / b)
+    elif valinta =="d":
+        print("kertolasku", a * b)
+    else:
+        print("virheellinen valinta")
 
 
